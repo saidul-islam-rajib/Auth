9471ff7ebf383ee5798b5ef3cd3c05d5acbdfccb
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2025-02-01 12:17:46 +0600

    add login and register file

--- AuthUI/src/app/components/login/login.component.ts
+++ AuthUI/src/app/components/login/login.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import ValidateForm from '../../helpers/validateForm';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa fa-eye-slash';
+  loginForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb?.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Login data : ', this.loginForm.value);
+      // send to backend
+
+      this.authService.login(this.loginForm.value)
+      .subscribe({
+        next: () => {
+          this.loginForm.reset();
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          alert(err.error.message);
+        }
+      })
+    } else {
+      console.log('invalid form');
+      ValidateForm.validateFormFields(this.loginForm);
+      alert('Your form is invalid');
+    }
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? (this.eyeIcon = 'fa-eye') : (this.eyeIcon = 'fa-eye-slash');
+    this.isText ? (this.type = 'text') : (this.type = 'password');
+  }
 }
--- AuthUI/src/app/components/register/register.component.ts
+++ AuthUI/src/app/components/register/register.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import ValidateForm from '../../helpers/validateForm';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa fa-eye-slash';
+  registerForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Valid form ', this.registerForm.value);
+      this.authService.register(this.registerForm.value)
+      .subscribe({
+        next: (response) => {
+          alert(response.message);
+          this.registerForm.reset();
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          alert(err.error.message);
+        }
+      })
+    } else {
+      ValidateForm.validateFormFields(this.registerForm);
+      alert('Invalid registration form');
+    }
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? (this.eyeIcon = 'fa-eye') : (this.eyeIcon = 'fa-eye-slash');
+    this.isText ? (this.type = 'text') : (this.type = 'password');
+  }
 }
